768a50a546e55e810081e540003159039e458032
developersung13 <developersung13@gmail.com>
2023-03-19 12:41:20 +0900

    [feat]: Swagger 추가로 인한 기존 '장소 관련 API' 스펙 기술을 위한 annotation 추가 (#2)

--- src/main/java/com/sinabro/server/controller/PlacesController.java
+++ src/main/java/com/sinabro/server/controller/PlacesController.java
@@ -2,24 +2,27 @@ package com.sinabro.server.controller;
 
 import com.sinabro.server.model.places.Places;
 import com.sinabro.server.model.places.PlacesDAO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Api(tags = {"장소 API"})
 public class PlacesController {
+
     @Autowired
     private PlacesDAO placesDAO;
     @GetMapping("/api/places")
+    @ApiOperation(value = "등록된 모든 장소 정보 조회", notes = "사용자에 의해 등록된 모든 장소의 정보를 조회합니다.", responseContainer = "List")
     public List<Places> getAllPlaces() {
         return placesDAO.getAllPlaces();
     }
 
     @PostMapping("/api/places/save")
+    @ApiOperation(value = "장소 등록", notes = "마커 위치에 대한 장소 정보를 등록합니다.", responseContainer = "List")
     public Places save(@RequestBody Places place) {
         return placesDAO.save(place);
     }
