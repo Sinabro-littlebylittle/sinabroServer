558cea4b3984691b3554aa65e89ac77d09704939
YEOLAM <developersung13@gmail.com>
2023-05-29 10:48:11 +0900

    [fix]: Place 스키마 versionKey 비활성화 (#13) (#14)

--- models/place.js
+++ models/place.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
-const PlaceSchema = new mongoose.Schema({
-  placeName: String,
-  address: String,
-  latitude: String,
-  longitude: String,
-});
+const PlaceSchema = new mongoose.Schema(
+  {
+    placeName: String,
+    address: String,
+    latitude: String,
+    longitude: String,
+  },
+  {
+    versionKey: false,
+  }
+);
 
 const Place = mongoose.model('Place', PlaceSchema);
 
