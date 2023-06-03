73d60eb5c4d72a5e8b5fb85fea8646b9051c3e96
developersung13 <developersung13@gmail.com>
2023-06-03 11:46:27 +0900

    [feat]: 'markers' collection 추가로 인한 'places' collection에 대한 model 필드 코드 수정 (#15)

--- models/place.js
+++ models/place.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const placeSchema = new mongoose.Schema(
   {
+    markerId: { type: Schema.Types.ObjectId, ref: 'markers' },
     placeName: String,
     address: String,
     detailAddress: String,
-    latitude: String,
-    longitude: String,
   },
   {
     versionKey: false,
