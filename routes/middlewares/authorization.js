c120abd8f78e1e37ff7f1884a8136173288d4a6b
developersung13 <developersung13@gmail.com>
2023-07-23 00:35:06 +0900

    [feat]: JWT 유효성 검증 middleware 파일 추가 (#26)

---
+++ routes/middlewares/authorization.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const verifyToken = (req, res, next) => {
+  try {
+    console.log(req.headers.cookie.replace('user=', ''));
+    const clientToken = req.headers.cookie.replace('user=', '');
+    const decoded = jwt.verify(clientToken, process.env.JWT_TOKEN_SECRET);
+    if (!decoded) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    res.locals.sub = decoded.id;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Token expried' });
+  }
+};
+
+exports.verifyToken = verifyToken;
