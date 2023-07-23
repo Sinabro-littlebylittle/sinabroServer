09e21a639d84953e24b6402cabad26a110fea985
developersung13 <developersung13@gmail.com>
2023-07-23 23:31:34 +0900

    [fix]: 'people_numbers' Schema에 대한 versionKey 옵션 생성 허용 (#30)

--- models/people_number.js
+++ models/people_number.js
@@ -1,16 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const peopleNumberSchema = new Schema(
-  {
-    placeId: { type: Schema.Types.ObjectId, ref: 'places' },
-    peopleCount: Number,
-    createdTime: String,
-  },
-  {
-    versionKey: false,
-  }
-);
+const peopleNumberSchema = new Schema({
+  placeId: { type: Schema.Types.ObjectId, ref: 'places' },
+  peopleCount: Number,
+  createdTime: String,
+});
 
 const PeopleNumber = mongoose.model('people_number', peopleNumberSchema);
 
